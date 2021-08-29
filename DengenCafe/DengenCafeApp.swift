8ad96644589852891a6b9d13f718453655d8d1fd
Tomohiro Sasaki <takatobi0326@icloud.com>
2021-08-29 15:44:41 +0900

    Initial Commit

---
+++ DengenCafe/DengenCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DengenCafeApp.swift
+//  DengenCafe
+//
+//  Created by user on 2021/08/29.
+//
+
+import SwiftUI
+
+@main
+struct DengenCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
